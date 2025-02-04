b959327e74c7d5b7439e1ea474b2407525ca9328
hoki9797 <hoki9797@gmail.com>
2025-02-04 09:04:12 +0900

    05응용 2번

--- part2/05.응용/(lv.3)날짜-사이-일수-계산하기.js
+++ part2/05.응용/(lv.3)날짜-사이-일수-계산하기.js
@@ -12,7 +12,13 @@
  * @returns {number}
  */
 
-function daysBetween(date1, date2) {}
+function daysBetween(date1, date2) {
+    let d1 = new Date(date1)
+    let d2 = new Date(date2)
+    let diff = Math.abs(d1.getTime() - d2.getTime())
+    let days = diff / (1000*60*60*24)
+    return days
+}
 
 // export 를 수정하지 마세요.
 export { daysBetween };
